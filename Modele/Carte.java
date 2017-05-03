7b115d75024956817985d38c8cd998cb71de0d67
x5pid <x5pid.series@gmail.com>
2017-05-03 16:36:17 +0200

    Ajout des Tas

--- Modele/Carte.java
+++ Modele/Carte.java
@@ -1,4 +1,32 @@
 package Modele;
 
-public class Carte {
+public enum Carte {
+	
+	UN(1),
+	DEUX(2),
+	TROIS(3),
+	QUATRE(4),
+	CINQ(5);
+	
+	int contenu;
+	
+	Carte(int i){
+		this.contenu = i;
+	}
+	
+	public boolean equals(Carte c) {
+		return (this.contenu == c.contenu);
+	}
+
+	public String toString(){
+		switch (this.contenu) {
+	        case 1: return "un";
+	        case 2: return "deux";
+	        case 3: return "trois";
+	        case 4: return "quatre";
+	        case 5: return "cinq";
+	        default: throw new RuntimeException("Ajout de carte sur " + this +" impossible");
+		}
+	}
+	
 }
