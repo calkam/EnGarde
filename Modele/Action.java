cba98cc9cd84e5103117fb67664e7b6645e55a18
x5pid <x5pid.series@gmail.com>
2017-05-03 17:16:40 +0200

    Modif Rapide

--- Modele/Action.java
+++ Modele/Action.java
@@ -8,16 +8,6 @@ public abstract class Action {
 	
 	
 	/** 
-	 * Test à partir d'une carte de la main du joueur, de la position de sa figurine
-	 * et de celle de son adversaire si l'action est possible
-	 * 
-	 * @param maCarte
-	 * @param maFigurine
-	 * @param figurineAdverse
-	 */
-	public abstract void estPossible(Carte maCarte, Figurine maFigurine, Figurine figurineAdverse);
-	
-	/** 
 	 * Si l'action est possible, retourne l'indice de la case où il est possible de se rendre
 	 * 
 	 * @param maCarte
--- Modele/Avancer.java
+++ Modele/Avancer.java
@@ -1,16 +1,10 @@
 package Modele;
 
 public class Avancer extends Action {
-
-	@Override
-	public void estPossible(Carte maCarte, Figurine maFigurine, Figurine figurineAdverse) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 	@Override
 	public int casePossible(Carte maCarte, Figurine maFigurine, Figurine figurineAdverse) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 }
